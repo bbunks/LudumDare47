3b1d3f0d04a6ce053c5cbb077979395f876cc26f
Dandala <33705402+Dandala88@users.noreply.github.com>
2020-10-03 17:04:04 -0600

    Added inventory management script

---
+++ Assets/Scripts/InventoryManagement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManagement : MonoBehaviour
+{
+    public PlayerCollision collisions;
+    public bool dash = false, roll = false, walljump = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i < collisions.getCollisions().Length; i++) {
+            if(collisions.getCollisions()[i] != null) {
+                if(collisions.getCollisions()[i].tag == "collectible") {
+                    switch(collisions.getCollisions()[i].name) {
+                        case "dash" :
+                            Debug.Log("Collected Dash");
+                            Destroy(GameObject.Find(collisions.getCollisions()[i].name));
+                            dash = true;
+                        break;
+                        case "roll" :
+                            Debug.Log("Collected Dash");
+                            Destroy(GameObject.Find(collisions.getCollisions()[i].name));
+                            roll = true;
+                        break;
+                        case "walljump" :
+                            Debug.Log("Collected Dash");
+                            Destroy(GameObject.Find(collisions.getCollisions()[i].name));
+                            walljump = true;
+                        break;
+                        default :
+                            Debug.Log("Not a known collectible");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
