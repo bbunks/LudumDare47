dc0f4f2fc2e1ba72cfaaa304add86dec664abc20
Brenden Bunker <brenden.bunker@gmail.com>
2020-10-02 20:06:28 -0600

    Basic Character Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;   
+
+    public float acceleration = 0.1f, speed = 6f, turnSmooth = 0.05f;
+    private float turnSmoothVelocity, timeSinceLastUse = 0;
+    private Vector3 playerVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+        if (direction.magnitude > 0.05)
+        {
+            if (timeSinceLastUse > 2)
+            {
+                //Look towards where the player is moving
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmooth);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+            else
+            {
+                //Look towards where the camera is looking
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmooth);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+        }
+
+        Vector3 moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized * speed * direction.magnitude;
+        playerVelocity = new Vector3(Mathf.Lerp(playerVelocity.x, moveDir.x, acceleration), playerVelocity.y, Mathf.Lerp(playerVelocity.z, moveDir.z, acceleration));
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
